4e1065009f3f9901f8c0aa632b3b09497076a90b
Sérgio Dantas <sergio@maismatematica.com.br>
2019-01-13 08:54:21 -0200

    Variáveis assossiadas a elementos do html - aulas 28 a 31

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birty = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+console.log(gender);
