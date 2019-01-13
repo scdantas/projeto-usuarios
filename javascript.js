80586a6895681931750cc0553599c9680bf5e258
Sérgio Dantas <sergio@maismatematica.com.br>
2019-01-13 12:24:19 -0200

    Variáveis assossiadas a elementos do html - aulas 28 a 31 (Variáveis associadas a elementos do html - aulas 28 a 31)

--- javascript.js
+++ javascript.js
@@ -1,9 +1,51 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birty = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
-console.log(gender);
+function addLine(dataUser) { //____________________________________________________função para criar uma linha de dados na tabela
+ 
+    document.getElementById("table-users").innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+}
+
+document.getElementById("form-user-create").addEventListener("submit",function(event){
+    
+    event.preventDefault(); //____________________________________________________cancela o comportamento padrão do formulário
+   
+    fields.forEach(function(field, index){ //_____________________________________percorre o formulário e cria um objeto com campos e valores
+    
+        if (field.name == "gender"){
+    
+            if(field.checked){
+                user[field.name] = field.value;
+            }
+     
+        } else {
+            user[field.name] = field.value;
+        }
+
+    });
+
+    var objectUser = new User( //_______________________________________________instancia um objeto User aplicado aos campos de 'user'
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    );
+
+    addLine(objectUser);
+});
